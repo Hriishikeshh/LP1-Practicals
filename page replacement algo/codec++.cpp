7decd8b6c3756148a9777c280b2f8c85a512abe9
hrishikesh <patkarhrishikesh04@gmail.com>
2024-10-06 17:47:31 +0530

    adding page replacement

---
+++ page replacement algo/codec++.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include<queue>
+#include<unordered_map>
+using namespace std;
+
+class pageReplacement{
+    queue<int> frame;
+    int pageFaults;
+    unordered_map<int,int> mp;
+
+    public:
+    // FIFO page replacement algorithm
+    void FIFO(vector<int>v,int pageframes){
+        pageFaults=0;
+        for(int i=0;i<v.size();i++){
+            // If page is not present in the frame
+            if(mp[v[i]]==0){
+                // If frame is full
+                if(frame.size()==pageframes){
+                    int temp=frame.front();
+                    frame.pop();
+                    mp[temp]=0;
+                }
+                // Push the page into the frame
+                frame.push(v[i]);
+                mp[v[i]]=1;
+                pageFaults++;
+            }
+        }
+        cout<<"Page Faults: "<<pageFaults<<endl;
+    }
+
+    // LRU page replacement algorithm
+};
+
+
+int main(){
+    cout<<"PAGE REPLACEMENT ALGORITHMS"<<endl;
+    vector<int> pages = {1,3,0,3,5,6,3};
+    int pageFrames=3;
+    pageReplacement pr;
+    pr.FIFO(pages,pageFrames);
+}
